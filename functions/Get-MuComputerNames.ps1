bbc468f691a9591c1529f9fee324e9c54b49c030
matt penny <matt.penny@simplyhealth.co.uk>
2024-10-03 13:47:58 +0000

    New thing to find names, and more significantly the spellings of names, of 'puters

---
+++ functions/Get-MuComputerNames.ps1
@@ -0,0 +1,31 @@
+﻿function Get-MuComputerNames {
+    <#
+.SYNOPSIS
+    Gets names from AD
+#>
+    [CmdletBinding()]
+    param (
+        $ComputerName
+    )
+
+    $DebugPreference = $PSCmdlet.GetVariableValue('DebugPreference')
+
+    write-startfunction
+
+    $ADComputers = Get-ADComputer -Filter * -Properties * |
+        Where-Object DNSHostName -Like "*$ComputerName*"
+
+    foreach ($C in $ADComputers) {
+        [string]$DnsHostName = $C.DNSHostName
+
+        $DnsRecord = Resolve-DnsName $DnsHostName
+
+        [PSCustomObject]@{
+            ComputerName = $C.DNSHostName
+            IPAddress    = $DnsRecord.IPAddress
+        }
+    }
+    write-endfunction
+
+
+}
