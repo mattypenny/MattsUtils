12a2e62d121da291ca909a1e3be0e76fb57da5ee
mattypenny_on_lulu <mattypenny@gmail.com>
2025-03-24 17:48:31 +0000

    more-or-less working

--- functions/search-MuSpotifyItems.ps1
+++ functions/search-MuSpotifyItems.ps1
@@ -1,3 +1,4 @@
+import-module -force Spotishell
 function Search-MuSpotifyItems {
     <#
 .SYNOPSIS
@@ -16,9 +17,10 @@ function Search-MuSpotifyItems {
 
     # foreach ($t in $(search-Item 'hilly fields nick' -type Track -ApplicationName spotishell)) { foreach ($i in $($t | select -expand tracks | select -expand items)) {foreach ($a in $($i | select -expand artists))  { [PSCustomObject]@{artist=$a.name; track = $i.name; type = $i.type}}}}
 
-    write-host 'welp'
+    . C:\Users\matty\OneDrive\backups\backup_of_repair_websites\Documents\PowerShell\Modules\Spotishell\1.1.1\Private\Send-SpotifyCall.ps1
+    
     $tracks = search-Item $SearchString -type Track -ApplicationName spotishell
-    write-dbg "`$tracks count: <$($tracks.Length)>"
+    write-dbg "`$tracks returned from search count: <$($tracks.Length)>"
 
     $songs = foreach ($t in $tracks) {
 
@@ -26,14 +28,20 @@ function Search-MuSpotifyItems {
         write-dbg "`$trackitems count: <$($trackitems.Length)>"
 
         foreach ($i in $trackItems) {
+            $AlbumStuff = $i | select -expand album
+            
+
             $artists = $i | select -expand artists
             write-dbg "`$artists count: <$($artists.Length)>"
 
             foreach ($a in $artists) {
                 [PSCustomObject]@{
-                    artist = $a.name
-                    track  = $i.name
-                    type   = $i.type
+                    artist   = $a.name
+                    track    = $i.name
+                    type     = $i.type
+                    album    = $AlbumStuff.name
+                    released = $AlbumStuff.release_date
+                    trackid  = $i.id
                 }
             }
 
