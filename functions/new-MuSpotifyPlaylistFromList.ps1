8d2c228a6d60f42ac6bc72cefd87b57d6d57fbc8
mattypenny_on_lulu <mattypenny@gmail.com>
2025-09-09 18:02:29 +0100

    renamed and made it either a file or a list of songs

--- functions/new-MuSpotifyPlaylistFromList.ps1
+++ functions/new-MuSpotifyPlaylistFromList.ps1
@@ -1,4 +1,4 @@
-function New-MuSpotifyPlaylistFromFile {
+function New-MuSpotifyPlaylistFromList {
     <#
 .SYNOPSIS
    Read a file containing a list of tracks, and create a playlist from the tracks in the file. 
@@ -11,7 +11,8 @@ function New-MuSpotifyPlaylistFromFile {
 #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $True)][string]$FileName,
+        [Parameter(Mandatory = $False)][string]$FileName,
+        [Parameter(Mandatory = $False)][string[]]$ListOfSongs,
         [Parameter(Mandatory = $True)][string]$PlaylistName,
         [string]$ApplicationName = 'spotishell',
         [string]$PlaylistFolder = 'Created by spotishell',
@@ -32,24 +33,36 @@ function New-MuSpotifyPlaylistFromFile {
     }
     $playlist = New-MuSpotifyPlaylist @NewPlaylistParams
    
-
-    # reading the file
-    $fileContent = Get-Content $FileName
-
-    # for each line in the file, search for the track, output the list of tracks with an sequence number, and the
-    # user can select which track best matches the line in the file (or none)
-    $tracks = foreach ($line in $fileContent) {
-
-        if (!($line)) {
-            continue
+    if ($Filename) {
+        if (-not (Test-Path $FileName)) {
+            throw "File $FileName does not exist"
         }
-        if ($line -match '^\s*#') {
-            continue
+        # reading the file
+        $fileContent = Get-Content $FileName
+
+        $ListOfSongs = foreach ($line in $fileContent) {
+
+            if (!($line)) {
+                continue
+            }
+            if ($line -match '^\s*#') {
+                continue
+            }
+            if ($line -match '^\s*$') {
+                continue
+            }
+            write-dbg "`$line: <$line>"
+
+            [PSCustomObject]@{
+                Song = $Line
+            }
         }
-        if ($line -match '^\s*$') {
-            continue
-        }
-        write-dbg "`$line: <$line>"
+    }
+    if (-not $ListOfSongs) {
+        throw "No songs found in file $FileName"
+    }    
+
+    foreach ($line in $ListOfSongs) {
         $SplatParameters = @{
             SearchString    = $line
             ShowFirstHits   = 5
